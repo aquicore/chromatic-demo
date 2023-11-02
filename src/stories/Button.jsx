14e65c5371be6a38027a6c2bffed9ac328a4193a
Ian Wright <ian@infogrid.io>
2023-11-02 10:59:31 +0000

    Initial commit

---
+++ src/stories/Button.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import React, { useCallback, useState } from "react";
+import "./button.css";
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ primary, backgroundColor, size, label, onClick, clickedText, ...props }) => {
+  const mode = primary ? "storybook-button--primary" : "storybook-button--secondary";
+
+  const [clicked, setClicked] = useState(false);
+
+  const internalOnClick = useCallback(() => {
+    setClicked(true);
+    onClick();
+  }, [onClick]);
+
+  return (
+    <>
+      <button
+        type="button"
+        className={["storybook-button", `storybook-button--${size}`, mode].join(" ")}
+        style={backgroundColor && { backgroundColor }}
+        {...props}
+        onClick={internalOnClick}
+      >
+        {label}
+      </button>
+      {clicked && <p>{clickedText}</p>}
+    </>
+  );
+};
+
+Button.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: "medium",
+  onClick: undefined,
+};
